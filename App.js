1c835997fcfe1bcbf17e4dc3c689723b6a9812cf
Apoorva Kumar .P <bpapoorva@gmail.com>
2024-02-24 10:27:02 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,10 @@
+  const heading = React.createElement('div', {
+      className: 'parent'
+    },
+    React.createElement('div', {
+      className: 'child',
+    }, [
+      React.createElement('h1', {}, 'I am a h1'), React.createElement('h2', {}, 'I am a h2')
+    ]));
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(heading);
