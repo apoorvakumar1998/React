7e43565fa1dc5e481def17d72003b0f58c55a7ae
Apoorva Kumar .P <bpapoorva@gmail.com>
2024-03-08 22:10:07 +0530

    updated about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,9 @@
-import UserClass from './UserClass';
 
 const About = () => {
   return (
     <div className="about-container text-center border border-solid border-black p-4 m-4 mx-auto">
-      <h1>About us</h1>
-      <h2>This a swiggy clone app</h2>
-      <UserClass name="virat kohli" />
+      <h1 className="font-bold text-2xl mb-10">About us</h1>
+      <h2>This a swiggy clone app called 'Eat Easy' build using real time Swiggy APIs</h2>
     </div>
 
   )
