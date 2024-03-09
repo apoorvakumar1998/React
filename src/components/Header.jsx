636dc25c8520fdfff4dda589d9674c1559d231a4
Apoorva Kumar .P <bpapoorva@gmail.com>
2024-03-09 15:05:30 +0530

    css changes for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,34 +9,33 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
   const { userName } = useContext(UserContext);
   const cartItems = useSelector((store) => store.cart.items);
-  const itemCount = cartItems.reduce((acc, curr) => {
-    return curr.quantity + acc
-  }, 0)
+  const itemCount = cartItems.reduce((acc, curr) => curr.quantity + acc, 0);
+
   return (
-    <div className="flex justify-between items-center sticky top-0 bg-yellow-100 border-yellow-400 z-[999] border-b-2">
+    <header className="flex justify-between items-center sticky top-0 bg-yellow-100 border-yellow-400 z-[999] border-b-2 px-4 py-2">
       <div className="logo-container flex items-center">
-        <img src={LOGO_URL} alt="Logo" className="w-16" />
+        <img src={LOGO_URL} alt="Logo" className="w-16 h-16" />
         <div className='font-bold text-xl ml-4 font-mono hover:italic text-yellow-400'>Eat-Easy</div>
       </div>
-      <div className="nav-items">
-        <ul className='flex items-center justify-center gap-8 pr-20 cursor-pointer'>
+      <nav className="nav-items">
+        <ul className='flex items-center justify-center gap-8 cursor-pointer'>
           <li>
             {onlineStatus ? 'Online✅' : 'Offline❌'}
           </li>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="hover:text-yellow-500">Home</Link>
           </li>
           <li>
-            <Link to="about">About Us</Link>
+            <Link to="/about" className="hover:text-yellow-500">About Us</Link>
           </li>
           <li>
-            <Link to="cart">Cart ({itemCount})</Link>
+            <Link to="/cart" className="hover:text-yellow-500">Cart ({itemCount})</Link>
           </li>
           <li className='text-xl font-bold'>{userName}</li>
         </ul>
-      </div>
-    </div>
-  )
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
